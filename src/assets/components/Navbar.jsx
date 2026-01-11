fe9aafd513cb7302a8ae82bd4314a90762906ecf
ana-morandeira <ana.agrodorolo@gmail.com>
2026-01-11 20:26:58 +0100

    feat/Navbar component and style

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">Aromatic Plants</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
