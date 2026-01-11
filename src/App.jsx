3abb16c54abe02a0574736b60bc1be5eea7d587d
ana-morandeira <ana.agrodorolo@gmail.com>
2026-01-11 13:32:00 +0100

    fix/app jsx:Render Aromatics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import Aromatics from "./assets/components/Aromatics";
 
+function App() {
+  return (
+    <div>
+      <h1>Top 5 de flores aromáticas</h1>
+      <Aromatics />
+    </div>
+  );
+}
+
+export default App;
